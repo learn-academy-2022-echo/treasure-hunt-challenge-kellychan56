a30705170c18200e41ae5e2a42d391471f0b7d52
kellychan56 <kelly.chan568@gmail.com>
2022-08-19 10:15:30 -0700

    created a 3x3 board

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 
-class Square extends Component {
-  render() {
+const Square = (props) => {
+
     return(
       <>
+        <div className="square">{props.value}</div>
+
       </>
     )
   }
-}
+
 export default Square
